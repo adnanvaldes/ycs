0c292e7dfa33b1325d45f459b0349ddb53358745
Adnan Valdes <adravaldes+github@gmail.com>
2021-07-16 18:04:04 -0700

    Add env variables

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY requirements.txt .
 RUN pip install --user -r requirements.txt
 
 FROM python:3.8-slim
+
+
 WORKDIR /code
 
 # copy only the dependencies installation from the 1st stage image
@@ -12,5 +14,17 @@ COPY ./src .
 
 RUN apt-get update && apt-get upgrade --no-install-recommends --no-install-suggests -y
 
+ARG UP_DURATION=60
+ENV upDuration=${UP_DURATION}
+ARG DOWN_DURATION=60
+ENV downDuration=${DOWN_DURATION}
+ARG LAT=49.217876
+ENV lat=${LAT}
+ARG LNG=-123.142097
+ENV lng=${LNG}
+ARG MORNING=sunrise
+ENV morning=${MORNING}
+ARG EVENING=civil_twilight_end
+ENV evening=${EVENING}
 ENV PATH=/root/.local:$PATH
 CMD [ "python", "./app.py" ]
--- src/app.py
+++ src/app.py
@@ -5,7 +5,22 @@ import requests
 import schedule
 import logging
 import time
+import os
 
+try:
+    upDuration = os.environ['upDuration']
+    downDuration = os.environ['downDuration']
+    lat = os.environ['lat']
+    lng = os.environ['lng']
+    morning_start = os.environ['morning']
+    evening_start = os.environ['evening']
+except KeyError:
+    upDuration = 60
+    downDuration = 60
+    lat = 49.217876
+    lng = -123.142097
+    morning_start = 'sunrise'
+    evening_start = 'civil_twilight_end'
 
 def to_time(time_string):
     times = datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%S+00:00").time()
@@ -15,18 +30,28 @@ def minutes_to_ms(minutes):
     return minutes * 60000
 
 @docker_log()
-def light_scheduler(latlng: tuple=(49.217876, -123.142097)):
-    sunTimes = requests.get(f'https://api.sunrise-sunset.org/json?lat={latlng[0]}&lng={latlng[1]}& formatted=0')
+def light_scheduler(lat, lng, m_start, n_start):
+    sunTimes = requests.get(f'https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}& formatted=0')
 
     if sunTimes.status_code == 200:
         sunTimes = sunTimes.json()['results']
-        morning = to_time(sunTimes['sunrise'])
-        evening = to_time(sunTimes['civil_twilight_end'])
+        try:
+            m_start = int(m_start)
+        except ValueError:
+            print('Morning start time is string, using API reference time.')
+            morning = to_time(sunTimes[m_start])
+            pass
 
+        try:
+            n_start = int(n_start)
+        except ValueError:
+            print('Evening start time is string, using API reference time.')
+            evening = to_time(sunTimes[n_start])
+            pass
 
-        schedule.every().day.at(morning).do(start_winding, up=True).tag("Morning routine")
+        schedule.every().day.at(morning).do(start_winding, up=True, upDuration=upDuration).tag("Morning routine")
         print(f"Sunrise routine scheduled at {morning}")
-        schedule.every().day.at(evening).do(start_winding, down=True).tag("Evening routine")
+        schedule.every().day.at(evening).do(start_winding, down=True, downDuration=downDuration).tag("Evening routine")
         print(f"Sunset routine scheduled at {evening}")
 
         return sunTimes
@@ -36,7 +61,7 @@ def light_scheduler(latlng: tuple=(49.217876, -123.142097)):
 
 
 @docker_log()
-def start_winding(up=False, upDuration:"minutes"=60, down=False, downDuration:"minutes"=0.5):
+def start_winding(upDuration:"minutes",downDuration:"minutes", up=False, down=False):
     for bulb in discover_bulbs():
         if up:
             print(f"Beginning wind-up routine - complete at {datetime.utcnow() + timedelta(minutes=upDuration)}")
@@ -58,12 +83,13 @@ def main():
     schedule_logger = logging.getLogger('schedule')
     schedule_logger.setLevel(level=logging.DEBUG)
 
-    schedule.every().day.at("10:00").do(light_scheduler).tag("Main scheduler")
+    schedule.every().minute.at(":30").do(light_scheduler, lat=lat, lng=lng, m_start=morning_start, n_start=evening_start).tag("Main scheduler")
     schedule.every(5).minutes.do(ping_bulbs).tag("Ping bulbs")
     while True:
         schedule.run_pending()
         all_jobs = schedule.get_jobs()
         print(all_jobs)
+        print(upDuration, downDuration, lat, lng)
         time.sleep(5)
 
 if __name__ == "__main__":
