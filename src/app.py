3cb874309087d9c19ea8ff22887fb9ad6d0b92e2
Adnan Valdes <adravaldes+github@gmail.com>
2021-07-16 19:05:10 -0700

    Update env variables

--- src/app.py
+++ src/app.py
@@ -5,22 +5,29 @@ import requests
 import schedule
 import logging
 import time
+import sys
 import os
 
 try:
-    upDuration = os.environ['upDuration']
-    downDuration = os.environ['downDuration']
-    lat = os.environ['lat']
-    lng = os.environ['lng']
-    morning_start = os.environ['morning']
-    evening_start = os.environ['evening']
+    upDuration = int(os.environ['ON_TRANSITION_PERIOD'])
+    downDuration = int(os.environ['OFF_TRANSITION_PERIOD'])
+    lat = os.environ['LAT']
+    lng = os.environ['LNG']
+    morning_start = os.environ['ON_START_TIME']
+    evening_start = os.environ['OFF_START_TIME']
+    update_time = os.environ['SUN_UPDATE_TIME']
+    ping_bulb_freq = int(os.environ['PING_BULB_FREQ'])
+
 except KeyError:
+    sys.stderr.write("ERROR - COULD NOT PARSE AT LEAST ONE ENV VARIABLE. USING DEFAULT VALUES.")
     upDuration = 60
     downDuration = 60
     lat = 49.217876
     lng = -123.142097
     morning_start = 'sunrise'
     evening_start = 'civil_twilight_end'
+    update_time = "10:00"
+    ping_bulb_freq = 15
 
 def to_time(time_string):
     times = datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%S+00:00").time()
@@ -31,28 +38,28 @@ def minutes_to_ms(minutes):
 
 @docker_log()
 def light_scheduler(lat, lng, m_start, n_start):
-    sunTimes = requests.get(f'https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}& formatted=0')
+    sunTimes = requests.get(f'https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}&formatted=0')
 
     if sunTimes.status_code == 200:
         sunTimes = sunTimes.json()['results']
         try:
             m_start = int(m_start)
         except ValueError:
-            print('Morning start time is string, using API reference time.')
+            sys.stdout.write('Morning start time is string, using API reference time.')
             morning = to_time(sunTimes[m_start])
             pass
 
         try:
             n_start = int(n_start)
         except ValueError:
-            print('Evening start time is string, using API reference time.')
+            sys.stdout.write('Evening start time is string, using API reference time.')
             evening = to_time(sunTimes[n_start])
             pass
 
         schedule.every().day.at(morning).do(start_winding, up=True, upDuration=upDuration).tag("Morning routine")
-        print(f"Sunrise routine scheduled at {morning}")
+        sys.stdout.write(f"Sunrise routine scheduled at {morning}")
         schedule.every().day.at(evening).do(start_winding, down=True, downDuration=downDuration).tag("Evening routine")
-        print(f"Sunset routine scheduled at {evening}")
+        sys.stdout.write(f"Sunset routine scheduled at {evening}")
 
         return sunTimes
 
@@ -64,12 +71,12 @@ def light_scheduler(lat, lng, m_start, n_start):
 def start_winding(upDuration:"minutes",downDuration:"minutes", up=False, down=False):
     for bulb in discover_bulbs():
         if up:
-            print(f"Beginning wind-up routine - complete at {datetime.utcnow() + timedelta(minutes=upDuration)}")
+            sys.stdout.write(f"Beginning wind-up routine - complete at {datetime.utcnow() + timedelta(minutes=upDuration)}")
             Bulb(bulb['ip'], effect='smooth', duration=minutes_to_ms(upDuration), auto_on=True).turn_on()
             return schedule.CancelJob
 
         elif down:
-            print(f"Beginning wind-down routine - complete at {datetime.utcnow() + timedelta(minutes=downDuration)}")
+            sys.stdout.write(f"Beginning wind-down routine - complete at {datetime.utcnow() + timedelta(minutes=downDuration)}")
             Bulb(bulb['ip'], effect='smooth', duration=minutes_to_ms(downDuration)).turn_off()
             return schedule.CancelJob
     return schedule.CancelJob
@@ -83,13 +90,11 @@ def main():
     schedule_logger = logging.getLogger('schedule')
     schedule_logger.setLevel(level=logging.DEBUG)
 
-    schedule.every().minute.at(":30").do(light_scheduler, lat=lat, lng=lng, m_start=morning_start, n_start=evening_start).tag("Main scheduler")
-    schedule.every(5).minutes.do(ping_bulbs).tag("Ping bulbs")
+    schedule.every().day.at(update_time).do(light_scheduler, lat=lat, lng=lng, m_start=morning_start, n_start=evening_start).tag("Main scheduler")
+    schedule.every(ping_bulb_freq).seconds.do(ping_bulbs).tag("Ping bulbs")
     while True:
         schedule.run_pending()
-        all_jobs = schedule.get_jobs()
-        print(all_jobs)
-        print(upDuration, downDuration, lat, lng)
+        schedule.get_jobs()
         time.sleep(5)
 
 if __name__ == "__main__":
