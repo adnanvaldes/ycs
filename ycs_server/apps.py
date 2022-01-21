07f07a34335a5ca823d0473ff1d1910b83fd1a35
AdnanValdes <adravaldes+github@gmail.com>
2022-01-21 11:04:56 -0800

    Remove Django framework from repo

--- ycs_server/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-from django.conf import settings
-
-class Ycs_serverConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ycs_server'
--- ycs_server/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def to_time(time_string):
-    times = datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%S+00:00").time()
-    return str(times)
-
-def minutes_to_ms(minutes):
-    return minutes * 60000
